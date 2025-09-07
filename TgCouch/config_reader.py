5c66bdc98af7318652d4fe9c8a3646daf8f7fef1
Nikola864 <kolyab89217115399@mail.ru>
2025-09-08 01:28:13 +0300

    new sub

--- TgCouch/config_reader.py
+++ TgCouch/config_reader.py
@@ -1,11 +1,14 @@
 from pydantic_settings import SettingsConfigDict, BaseSettings
 from pydantic import SecretStr
+import os
 
 class Settings(BaseSettings):
     bot_token: SecretStr
     admin_id: int
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(__file__), '.env'),  # <-- указывает путь к .env в той же папке, где config_reader.py
+        env_file_encoding='utf-8'
+    )
 
 config = Settings()
-
